b3655f57925802035c4ef03addf21be98d8a0b0c
“nik-d86” <nik-d86@yandex.ru>
2017-11-02 22:28:32 +0600

    Изменил индекс

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-﻿
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <title>Parallax</title>
+ <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<h3>привет</h3>
+</body>
+</html>
